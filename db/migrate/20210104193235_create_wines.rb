4c10a761bbc2b77b8ac9a57916c4fd65285d091e
github username <github email address>
2021-01-04 11:33:39 -0800

    created model wine

---
+++ db/migrate/20210104193235_create_wines.rb
@@ -0,0 +1,15 @@
+class CreateWines < ActiveRecord::Migration
+  def change
+    create_table :wines do |t|
+      t.string :brand
+      t.string :winery
+      t.string :state
+      t.string :county
+      t.string :varietal
+      t.date :vintage
+      t.integer :price
+
+      t.timestamps null: false
+    end
+  end
+end
