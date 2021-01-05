df5640990655dbdee72d360277890a0ffda8a125
github username <github email address>
2021-01-05 15:27:50 -0800

    added login view and login router

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,8 +14,23 @@ class SessionController < ApplicationController
         end
     end
 
+    get '/login' do
+        
+        erb :'sessions/login'
+    end
+
+    post '/login' do
+        user = User.find_by_username(params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect '/wines'
+        else
+            redirect '/login'
+        end
+    end
+
     get '/logout' do
         session.clear
-        redirect '/signup'
+        redirect '/login'
     end
 end
