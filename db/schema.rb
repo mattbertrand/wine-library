9da04de1a425e4c0fa121b0cab1045cc1b3678ac
github username <github email address>
2021-01-05 11:20:42 -0800

    create table user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210105191518) do
+ActiveRecord::Schema.define(version: 20210105191931) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20210105191518) do
     t.integer  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
 end
