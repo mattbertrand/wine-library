a61402152afd17626c9b5943c6b13c0d4cca431f
github username <github email address>
2021-01-05 11:16:03 -0800

    added users

--- config.ru
+++ config.ru
@@ -7,4 +7,5 @@ end
 use Rack::MethodOverride
 
 use WinesController
+use SessionController
 run ApplicationController
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210104193235) do
+ActiveRecord::Schema.define(version: 20210105191518) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "wines", force: :cascade do |t|
     t.string   "brand"
