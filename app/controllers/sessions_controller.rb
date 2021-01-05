bc9423ff7b149e2254d96c0f5ac7d742a9797644
github username <github email address>
2021-01-05 14:44:30 -0800

    added router post signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,16 @@ class SessionController < ApplicationController
     get '/signup' do
         
         erb :'sessions/signup'
-      end
+    end
+
+    post '/signup' do
+        user = User.new(params[:user])
+        if user.save
+            session[:user_id] = user.id
+            redirect '/wines'
+        else
+            redirect '/signup'
+        end
+        binding.pry
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
     has_many :wines
     has_secure_password
+
+    validates :username, presence: true, uniqueness: true
 end
