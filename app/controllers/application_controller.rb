29bd6d2ea1210af26b04c669bb1afb6c450a157b
github username <github email address>
2021-01-05 14:46:22 -0800

    added helper to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,14 @@ class ApplicationController < Sinatra::Base
     set :session_secret, ENV['SESSION_SECRET']
   end
 
+  helpers :is_logged_in?
+
   get "/" do
     erb :welcome
+  end
+
+  def is_logged_in?
+    !!session[:user_id]
   end
 
 end
